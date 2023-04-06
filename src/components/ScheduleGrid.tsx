b16f0a66625972f6e39b76a2fef166d05040d6e6
Cam <cameron.keif@gmail.com>
2023-04-06 11:27:55 -0400

    allow schedule to change time range

--- src/components/ScheduleGrid.tsx
+++ src/components/ScheduleGrid.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, ReactElement } from 'react';
+import { useState, useEffect, useCallback, ReactElement } from 'react';
 import axios from 'axios';
 import { DateTime } from 'luxon';
 import MlbDay from '../types/MlbDay';
@@ -61,11 +61,13 @@ const ScheduleGrid = () => {
     const [data, setData] = useState<Map<string, (MlbGame | null)[]> | null>(null);
     const [days, setDays] = useState<string[]>([]);
     const [todayIndex, setTodayIndex] = useState<number | null>(null);
+    const [dateRange, setDateRange] = useState<string>('2023-03-30;2023-04-09'); // TODO initialize this to the current week.
 
-    const getSchedule = async () => {
+    const getSchedule = useCallback(async () => {
         try {
             // TODO fix this hard coded value
-            const url = 'http://localhost:6060/schedule?startDate=2023-03-30&endDate=2023-04-09&includeOdds=false';
+            const [startDate, endDate] = dateRange.split(';');
+            const url = `http://localhost:6060/schedule?startDate=${startDate}&endDate=${endDate}&includeOdds=false`;
             const response = await axios.get(url);
             setData(parseScheduleData(response.data));
 
@@ -84,11 +86,11 @@ const ScheduleGrid = () => {
             alert('Something went wrong, check the console.');
             console.error(e);
         }
-    }
+    }, [dateRange]);
 
     useEffect(() => {
         getSchedule();
-    }, []);
+    }, [dateRange, getSchedule]);
 
     if (!data) {
         return null;
@@ -107,17 +109,46 @@ const ScheduleGrid = () => {
     });
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                    {days.map((day) => <th key={day}>{day}</th>)}
-                </tr>
-            </thead>
-            <tbody>
-                {scheduleRows}
-            </tbody>
-        </table>
+        <div>
+            <select onChange ={(event) =>{setDateRange(event.target.value)}}>
+                <option value="2023-03-30;2023-04-09">Week 1 (Mar 30 - Apr 9)</option>
+                <option value="2023-04-10;2023-04-16">Week 2 (Apr 10 - Apr 16)</option>
+                <option value="2023-04-17;2023-04-23">Week 3 (Apr 17 - Apr 23)</option>
+                <option value="2023-04-24;2023-04-30">Week 4 (Apr 24 - Apr 30)</option>
+                <option value="2023-05-01;2023-05-07">Week 5 (May 1 - May 7)</option>
+                <option value="2023-05-08;2023-05-14">Week 6 (May 8 - May 14)</option>
+                <option value="2023-05-15;2023-05-21">Week 7 (May 15 - May 21)</option>
+                <option value="2023-05-22;2023-05-28">Week 8 (May 22 - May 28)</option>
+                <option value="2023-05-29;2023-06-04">Week 9 (May 29 - Jun 4)</option>
+                <option value="2023-06-05;2023-06-11">Week 10 (Jun 5 - Jun 11)</option>
+                <option value="2023-06-12;2023-06-18">Week 11 (Jun 12 - Jun 18)</option>
+                <option value="2023-06-19;2023-06-25">Week 12 (Jun 19 - Jun 25)</option>
+                <option value="2023-06-26;2023-07-02">Week 13 (Jun 26 - Jul 2)</option>
+                <option value="2023-07-03;2023-07-09">Week 14 (Jul 3 - Jul 9)</option>
+                <option value="2023-07-10;2023-07-16">Week 15 (Jul 10 - Jul 16)</option>
+                <option value="2023-07-17;2023-07-23">Week 16 (Jul 17 - Jul 23)</option>
+                <option value="2023-07-24;2023-07-30">Week 17 (Jul 24 - Jul 30)</option>
+                <option value="2023-07-31;2023-08-06">Week 18 (Jul 31 - Aug 6)</option>
+                <option value="2023-08-07;2023-08-13">Week 19 (Aug 7 - Aug 13)</option>
+                <option value="2023-08-14;2023-08-20">Week 20 (Aug 14 - Aug 20)</option>
+                <option value="2023-08-21;2023-08-27">Week 21 (Aug 21 - Aug 27)</option>
+                <option value="2023-08-28;2023-09-03">Week 22 (Aug 28 - Sep 3)</option>
+                <option value="2023-09-04;2023-09-10">Week 23 (Sep 4 - Sep 10)</option>
+                <option value="2023-09-11;2023-09-17">Week 24 (Sep 11 - Sep 17)</option>
+                <option value="2023-09-18;2023-09-24">Week 25 (Sep 18 - Sep 24)</option>
+            </select>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        {days.map((day) => <th key={day}>{day}</th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    {scheduleRows}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
